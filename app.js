4e39f6f62534f9d4224bcd088dd6e7ce6313fc86
Evan Staton <statonse@gmail.com>
2017-03-17 21:36:47 -0700

    add about route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
 var index = require('./routes/index');
-//var users = require('./routes/users');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/users', users);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
