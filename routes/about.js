f1b8dcfbcd1b623e1706fdbd5aeb21bc9be3a771
Evan Staton <statonse@gmail.com>
2017-03-17 21:37:31 -0700

    render page not message

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET about listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+      //res.send('respond with a resource');
+      res.render('about', { title: 'Sunflower Genome Database' });
 });
 
 module.exports = router;
