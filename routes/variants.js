7850a8b35079812d0673cab4dc684456831fd671
Evan Staton <statonse@gmail.com>
2017-03-22 19:56:02 -0700

    add views/routes for data sources

---
+++ routes/variants.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET variants listing. */
+router.get('/variants', function(req, res, next) {
+      //res.send('respond with a resource');
+      res.render('variants', { title: 'Sunflower Genome Database' });
+});
+
+module.exports = router;
