c07cc5d40d85a00ec49fae1d66d50c720fbe4f78
Evan Staton <statonse@gmail.com>
2017-03-24 12:27:19 -0700

    add faq views/routes

---
+++ routes/faq.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET faq listing. */
+router.get('/faq', function(req, res, next) {
+      //res.send('respond with a resource');
+      res.render('faq', { title: 'Sunflower Genome Database' });
+});
+
+module.exports = router;
