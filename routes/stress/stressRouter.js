aec2a7eb29963690f409324e9cc7b290466c42c5
Evan Staton <statonse@gmail.com>
2017-03-30 12:35:18 -0700

    add stress subdomain

---
+++ routes/stress/stressRouter.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET about listing. */
+router.get('/', function(req, res, next) {
+      //res.send('respond with a resource');
+      res.render('stress/index', { title: 'Sunflower Genome Database' });
+});
+
+module.exports = router;
