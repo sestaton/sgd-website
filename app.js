049007996487e52b89f3ad9893ae71a5082074c8
Evan Staton <statonse@gmail.com>
2017-03-30 12:34:49 -0700

    add stress subdomain

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
-var requireDir = require('require-dir');
+var bodyParser   = require('body-parser');
+var helmet       = require('helmet');
+var requireDir   = require('require-dir');
+var subdomain    = require('express-subdomain');
+
+var app = express();
 
 // mount all routes http://stackoverflow.com/a/25446206
-var routes = requireDir('./routes'); // https://www.npmjs.org/package/require-dir
-var app = express();
+var stressRouter = require('./routes/stress/stressRouter');
+//var subdomainOpts = { base : 'localhost', removeWWW : true };
+app.use(subdomain('stress', stressRouter)); //(subdomainOpts));
+//app.use(require('subdomain')('stress', stressRouter)); //(subdomainOpts));
+
+var routes = requireDir('./routes', {recurse: false}); // https://www.npmjs.org/package/require-dir
 for (var i in routes) app.use('/', routes[i]);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-//app.set('view engine', 'html');
-//app.engine('html', require('ejs').renderFile);
 
 app.use(helmet());
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev')); // short
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(require('stylus').middleware(path.join(__dirname, 'dist')));
-//app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'dist')));
+
+//var subdomainOpts = { base : 'localhost', removeWWW : true };
+
+//app.use(subdomain('stress', stressRouter));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
