fedc8f74bcb7356852844b995c4163109064cb65
Evan Staton <statonse@gmail.com>
2017-04-05 14:55:20 -0700

    modify route for subapp

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(vhost('stress.localhost', stressRouter));
 
 // blast app is a Rails app running on a different port
 var blastRouter = require('./routes/blast/blastRouter');
-app.use('/blast', blastRouter);
+app.use(vhost('localhost/blast', blastRouter));
 
 // mount all routes http://stackoverflow.com/a/25446206
 var routes = requireDir('./routes', {recurse: false}); // https://www.npmjs.org/package/require-dir
@@ -24,6 +24,7 @@ for (var i in routes) app.use('/', routes[i]);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+//app.enable('view cache');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev')); // short
