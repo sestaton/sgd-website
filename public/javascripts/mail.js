6fa7a465e2e321a734e403182627a54deff7e91d
Evan Staton <statonse@gmail.com>
2017-04-05 14:32:55 -0700

    add methods for getting contact info

---
+++ public/javascripts/mail.js
@@ -0,0 +1,10 @@
+var vForm = document.getElementById('mgform');
+var vInput = document.getElementById('email');
+
+vForm.onsubmit = function() {
+  if (this.hid == 'submit') {
+    location = '/submit/' + encodeURIComponent(vInput.value);
+  }
+
+  return false;
+}
