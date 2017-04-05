ef82b3939dc7d67c06ae2d6ce274e03d80e01ffd
Evan Staton <statonse@gmail.com>
2017-04-05 14:34:07 -0700

    try to mount subapp

--- routes/blast/blastRouter.js
+++ routes/blast/blastRouter.js
@@ -3,11 +3,12 @@ var router = express.Router();
 
 var app = express();
 
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
       //res.send('respond with a resource');
       //res.render('about', { title: 'Sunflower Genome Database' });
       console.log("Request to /blast app");
 });
 
 app.listen(4567);
-module.exports = router;
+module.exports = app;
+//module.exports = router;
