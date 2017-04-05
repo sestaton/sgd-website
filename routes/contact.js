2b8eae1940dda54730c6d750bc8e7d0dda16ed77
Evan Staton <statonse@gmail.com>
2017-04-05 14:34:54 -0700

    update form submission and assets

--- routes/contact.js
+++ routes/contact.js
@@ -1,10 +1,55 @@
 var express = require('express');
+var Mailgun = require('mailgun-js')
+
 var router = express.Router();
 
-/* GET about listing. */
+//Your api key, from Mailgun’s Control Panel
+var api_key = 'key-e37b7da04b1d31e31cbc574be0ffae07';
+//Your domain, from the Mailgun Control Panel
+var domain = 'mail.sunflowergenome.org';
+//Your sending email address
+var from_who = '"SGD website" <contact@mail.sunflowergenome.org>';
+var recipient = 'statonse@protonmail.com';
+
 router.get('/contact', function(req, res, next) {
-      //res.send('respond with a resource');
-	res.render('contact', { title: 'Sunflower Genome Database' });
+	 res.render('contact', { title: 'Sunflower Genome Database' });
+});
+
+// Send a message to the specified email address when you navigate to /submit/someaddr@email.com
+// The index redirects here
+router.get('/submit/:mail', function(req, res) {
+    //We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+    var data = {
+      //Specify email data
+    	from: from_who,
+      //The email to contact
+      to: recipient,
+      //Subject and text data
+      subject: 'Message from SGD database contact form',
+			name: req.params.name,
+			contact: req.params.email,
+			phone: req.params.phone,
+      message: req.params.message
+    }
+
+    //Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            res.render('error', { error : err});
+            console.log("got an error: ", err);
+        }
+        //Else we can greet    and leave
+        else {
+            //Here "submitted.jade" is the view file for this landing page
+            //We pass the variable "email" from the url parameter in an object rendered by Jade
+            res.render('submitted', { name : req.params.name });
+            console.log(body);
+        }
+    });
+
 });
 
 module.exports = router;
