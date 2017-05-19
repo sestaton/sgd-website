74bb4883bfc8c31343f9f106834d234d2da1cdcb
Evan Staton <statonse@gmail.com>
2017-05-19 11:32:02 -0700

    clean up

--- fixlns.pl
+++ fixlns.pl
@@ -16,23 +16,19 @@ use File::Basename;
 use File::Path qw(make_path);
 use Cwd        qw(abs_path getcwd);
 use Getopt::Long;
-use Data::Dump::Color;
+#use Data::Dump::Color;
 
 my $usage = "\nUSAGE: ".basename($0)." -i <indir>";
 
 my %opts;
 GetOptions(\%opts, 'indir|i=s', 'outdir|o=s');
-#say STDERR $usage and exit(1) unless %opts;
+say STDERR $usage and exit(1) unless %opts;
 
 $opts{indir}  = abs_path($opts{indir});
 
 my $wd    = getcwd();
 my $base  = basename($opts{indir});
 my $dname = dirname($opts{indir});
-
-# Prepare assets
-#my $assetdir = File::Spec->catdir($opts{outdir}, 'assets'); 
-#make_path($assetdir, {verbose => 0, mode => 0771,});
 
 my (@files, @subdirs);
 find(sub { push @files, $File::Find::name if -d and ! /assets|stress/ }, $opts{indir});
@@ -42,10 +38,8 @@ die "\nERROR: No HTML files found in $opts{indir}. Exiting.\n"
 
 for my $in (@files) {
     next if $in eq $opts{indir};
-    #next if $in =~ /nav-|header|footer/; # Skip these templates. We can probably tell gulp to not compile them
-
     next if $in =~ /assets|stress|jbrowse/;
-    say $in;
+    say $in; # for debugging
 
     chdir $in or die $!;
     unlink('assets') if -e 'assets';
@@ -54,7 +48,7 @@ for my $in (@files) {
 }
 
 exit;
-
+## methods
 sub _copy_page {
     my ($file, $dest, $dname) = @_;
 
